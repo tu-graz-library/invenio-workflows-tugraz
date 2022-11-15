f055eefa7f5eb033e01f7418d258bb79f7340ca5
Christoph Ladurner <christoph.ladurner@tugraz.at>
2022-11-15 15:17:07 +0100

    init package

---
+++ AUTHORS.rst
@@ -0,0 +1,13 @@
+..
+    Copyright (C) 2022 Graz University of Technology.
+
+    invenio-workflows-tugraz is free software; you can redistribute it and/or
+    modify it under the terms of the MIT License; see LICENSE file for more
+    details.
+
+Authors
+=======
+
+This package is used to implement the workflows of the tu graz repository.
+
+- Graz University of Technology <info@tugraz.at>
---
+++ invenio_workflows_tugraz/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2022 Graz University of Technology.
+#
+# invenio-workflows-tugraz is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""This package serves as a place for the workflows of the repository of the TU Graz."""
+
+from .ext import InvenioWorkflowsTugraz
+
+__version__ = "0.0.0"
+
+__all__ = ("__version__", "InvenioWorkflowsTugraz")
