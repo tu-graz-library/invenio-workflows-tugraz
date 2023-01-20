8d0ae522f6dc7a904513d9125d0d29511710e707
Christoph Ladurner <christoph.ladurner@tugraz.at>
2023-01-20 10:29:19 +0100

    fix: remove print

--- invenio_workflows_tugraz/theses/theses.py
+++ invenio_workflows_tugraz/theses/theses.py
@@ -80,7 +80,6 @@ def theses_filter_for_open_records():
 
 def theses_create_aggregator():
     """This function returns a list of marc21 ids."""
-    print("---------- theses_create_aggregator -----------------")
     search = RecordsSearch(index="marc21records-drafts")
     query = {
         "must_not": [
