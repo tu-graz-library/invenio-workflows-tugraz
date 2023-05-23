06cd3bdcc178886d7e57ae40b5aacac2593a1324
Christoph Ladurner <christoph.ladurner@tugraz.at>
2023-05-23 23:32:14 +0200

    theses: rewrite access in update func

--- invenio_workflows_tugraz/theses/__init__.py
+++ invenio_workflows_tugraz/theses/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2022 Graz University of Technology.
+# Copyright (C) 2022-2023 Graz University of Technology.
 #
 # invenio-workflows-tugraz is free software; you can redistribute it and/or
 # modify it under the terms of the MIT License; see LICENSE file for more
@@ -12,6 +12,7 @@ from .theses import (
     import_func,
     theses_create_aggregator,
     theses_filter_for_open_records,
+    update_func,
 )
 from .views import create_blueprint
 
@@ -19,5 +20,6 @@ __all__ = (
     "theses_create_aggregator",
     "theses_filter_for_open_records",
     "import_func",
+    "update_func",
     "create_blueprint",
 )
