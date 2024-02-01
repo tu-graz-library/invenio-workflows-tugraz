2867b533b3b64d7e5ff9f564da3a3081b47c2204
Christoph Ladurner <christoph.ladurner@tugraz.at>
2024-02-01 21:01:40 +0100

    theses: add endpoint for theses uploads
    * with this endpoint it is possible to open the upload page of a record
      over the thesisid.

--- invenio_workflows_tugraz/theses/views.py
+++ invenio_workflows_tugraz/theses/views.py
@@ -2,7 +2,7 @@
 #
 # This file is part of Invenio.
 #
-# Copyright (C) 2022-2023 Graz University of Technology.
+# Copyright (C) 2022-2024 Graz University of Technology.
 #
 # Invenio-Records-Marc21 is free software; you can redistribute it and/or
 # modify it under the terms of the MIT License; see LICENSE file for more
@@ -11,7 +11,8 @@
 
 """Blueprint definitions."""
 
-from flask import Blueprint, Flask, redirect
+from flask import Blueprint, Flask, g, redirect
+from invenio_records_marc21.proxies import current_records_marc21
 from werkzeug.wrappers import Response as BaseResponse
 
 from .decorators import pass_record_from_pid
@@ -23,6 +24,14 @@ def record_from_pid(record: dict, **__: dict) -> BaseResponse:
     return redirect(record["links"]["self_html"], code=301)
 
 
+@pass_record_from_pid
+def uploads_from_thesesid(record: dict, **__: dict) -> BaseResponse:
+    """Upload introduced by theses id."""
+    marcid = record["id"]
+    current_records_marc21.records_service.edit(g.identity, marcid)
+    return redirect(f"/publications/uploads/{marcid}", code=301)
+
+
 def create_blueprint(_: Flask) -> Blueprint:
     """Register blueprint routes on app."""
     blueprint = Blueprint(
@@ -32,5 +41,6 @@ def create_blueprint(_: Flask) -> Blueprint:
     )
 
     blueprint.add_url_rule("/<path:pid_value>", view_func=record_from_pid)
+    blueprint.add_url_rule("/<path:pid_value>/uploads", view_func=uploads_from_thesesid)
 
     return blueprint
