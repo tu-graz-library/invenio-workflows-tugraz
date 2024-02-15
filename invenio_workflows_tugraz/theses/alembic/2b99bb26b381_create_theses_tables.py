68ac86fb183141e86dac5a96ff7bb160113b3085
Christoph Ladurner <christoph.ladurner@tugraz.at>
2024-02-15 09:45:44 +0100

    theses: add alembic

---
+++ invenio_workflows_tugraz/theses/alembic/2b99bb26b381_create_theses_tables.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2024 Graz University of Technology.
+#
+# invenio-workflows-tugraz is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Alembic create tables for theses workflow."""
+
+import sqlalchemy as sa
+import sqlalchemy_utils
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "2b99bb26b381"
+down_revision = None
+branch_labels = ()
+depends_on = "9d446c6a77e2"
+
+
+def upgrade():
+    """Upgrade database."""
+    op.create_table(
+        "workflows_theses",
+        sa.Column(
+            "id",
+            sqlalchemy_utils.types.uuid.UUIDType(),
+            nullable=False,
+        ),
+        sa.Column(
+            "cms_id",
+            sa.Integer(),
+            autoincrement=False,
+            nullable=False,
+        ),
+        sa.Column(
+            "archived",
+            sa.Boolean(),
+            default=False,
+        ),
+        sa.Column(
+            "published",
+            sa.Boolean(),
+            default=False,
+        ),
+        sa.Column(
+            "ready_to_archive",
+            sa.Boolean(),
+            default=False,
+        ),
+        sa.Column(
+            "ready_to_publish",
+            sa.Boolean(),
+            default=False,
+        ),
+        sa.PrimaryKeyConstraint("id", name=op.f("pk_workflows_theses")),
+    )
+
+
+def downgrade():
+    """Downgrade database."""
+    op.drop_table("workflows_theses")
---
+++ invenio_workflows_tugraz/theses/alembic/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2024 Graz University of Technology.
+#
+# invenio-workflows-tugraz is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Alembic for theses workflow."""
