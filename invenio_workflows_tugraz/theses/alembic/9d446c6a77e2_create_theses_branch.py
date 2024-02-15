5c01df79981ea87f74c446f1eb6ca2f55b693e1a
Christoph Ladurner <christoph.ladurner@tugraz.at>
2024-02-15 09:45:44 +0100

    theses: add alembic

---
+++ invenio_workflows_tugraz/theses/alembic/9d446c6a77e2_create_theses_branch.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2024 Graz University of Technology.
+#
+# invenio-workflows-tugraz is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Alembic create branch for theses workflow."""
+
+
+# revision identifiers, used by Alembic.
+revision = "9d446c6a77e2"
+down_revision = None
+branch_labels = ("workflows_theses",)
+depends_on = "dbdbc1b19cf2"
+
+
+def upgrade():
+    """Upgrade database."""
+
+
+def downgrade():
+    """Downgrade database."""
