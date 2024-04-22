93817457175b2f8caa8a0678990b0abecb36f43f
Christina Raab <christina.raab@tugraz.at>
2024-04-22 10:06:11 +0200

    oer: mapping lom -> marc21
    * this is the first step to create a workflow to import oer's into the library
      system alma.

--- invenio_workflows_tugraz/oer/convert.py
+++ invenio_workflows_tugraz/oer/convert.py
@@ -38,44 +38,158 @@ class Visitor:
 class LOM2Marc21(Visitor):
     """LOM to Marc21 converter class."""
 
-    def __init__(self, record: Marc21Metadata) -> None:
+    def __init__(self, record: Marc21Metadata, doi: str) -> None:
         """Construct."""
         super().__init__()
+        record.emplace_datafield("024.7.0.", subfs={"a": doi})
+        record.emplace_datafield("040.0.0.", subfs={"b": "ger", "e": "rda"})
+        record.emplace_datafield("044.0.0.", subfs={"c": "XA-AT"})
+        record.emplace_datafield("336.0.0.", subfs={"b": "txt"})
+        record.emplace_datafield("336.0.0.", subfs={"b": "tdi"})
+        record.emplace_datafield("337.0.0.", subfs={"b": "c"})
+        record.emplace_datafield("338.0.0.", subfs={"b": "cr"})
+        record.emplace_datafield("347.0.0.", subfs={"a": "Textdatei"})
+        record.emplace_datafield("347.0.0.", subfs={"a": "Videodatei"})
+        record.emplace_datafield("500.0.0.", subfs={"a": "Gesehen am"})
+        record.emplace_datafield(
+            "506.0.0.",
+            subfs={"f": "Unrestricted online access", "2": "star"},
+        )
+        record.emplace_datafield(
+            "655.0.7.",
+            subfs={
+                "a": "Massive Open Online Course",
+                "0": "$$0 (DE-588)107372798X",
+                "2": "gnd-content",
+            },
+        )
 
-        # TODO: add hardcoded categories
+        self.title = ""
+        self.entities = []
+
+    def visit(self, value: dict, record: Marc21Metadata) -> None:
+        """Visit."""
+        super().visit(value, record)
+
+        if self.title == "":
+            return
+
+        subfs = {"a": self.title}
+
+        if len(self.entities) > 0:
+            subfs["c"] = ",".join(self.entities)
+
+        record.emplace_datafield(
+            "245.0.0.",
+            subfs=subfs,
+        )
 
     def visit_general(self, value: dict, record: Marc21Metadata) -> None:
         """Process General."""
-        # TODO
+        super().visit(value, record)
 
-    def visit_title(self, value: dict, **__: dict) -> None:
+    def visit_title(self, value: dict, _: Marc21Metadata) -> None:
         """Process ."""
-        # TODO
+        self.title = value["langstring"]["#text"]
 
     def visit_language(self, value: str, record: Marc21Metadata) -> None:
         """Process ."""
-        # TODO
+        for language in value:
+            record.emplace_datafield("041.0.0.", subfs={"a": language})
 
     def visit_description(self, value: list, record: Marc21Metadata) -> None:
         """Process ."""
-        # TODO
+        for description in value:
+            text = description["langstring"]["#text"]
+            record.emplace_datafield("520.0.0.", subfs={"a": text})
+
+    def visit_educational_description(
+        self,
+        value: list,
+        record: Marc21Metadata,
+    ) -> None:
+        """Process ."""
+        for description in value:
+            text = description["langstring"]["#text"]
+            record.emplace_datafield("521.0.0.", subfs={"a": text})
 
     def visit_technical(self, value: dict, record: Marc21Metadata) -> None:
         """Process technical."""
-        # TODO
+        super().visit(value, record)
+
+    def visit_thumbnail(self, value: dict, record: Marc21Metadata) -> None:
+        """Process thumbnail."""
+        super().visit(value, record)
+
+    def visit_licenses(self, value: dict, record: Marc21Metadata) -> None:
+        """Process licenses."""
+        try:
+            id_ = value["id"]
+        except KeyError:
+            id_ = None
+
+        try:
+            url = value["url"]
+        except KeyError:
+            url = None
+
+        if id_ is None or url is None:
+            return
+
+        record.emplace_datafield(
+            "540.0.0.",
+            subfs={
+                "2": "cc",
+                "a": "Thumbnail",
+                "f": id_,
+                "u": url,
+            },
+        )
 
     def visit_location(self, value: dict, record: Marc21Metadata) -> None:
         """Process location."""
-        # TODO
+        text = value["#text"]
+        record.emplace_datafield(
+            "856.4.0.",
+            subfs={"u": text, "x": "IMOOX", "z": "kostenfrei", "3": "Volltext"},
+        )
+
+    def visit_duration(self, value: dict, record: Marc21Metadata) -> None:
+        """Process duration."""
+        # TODO nachfragen
+        # record.emplace_datafield("300.0.0.", subfs={"a": "Online-Ressource"})
 
     def visit_educational(self, value: dict, record: Marc21Metadata) -> None:
         """Process educational."""
-        # TODO
+        if "description" in value:
+            self.visit_educational_description(value["description"], record)
+            del value["description"]
+
+        super().visit(value, record)
 
     def visit_rights(self, value: dict, record: Marc21Metadata) -> None:
         """Process rights."""
-        # TODO
+        super().visit(value, record)
+
+    def visit_url(self, value: str, record: Marc21Metadata) -> None:
+        """Process url."""
+        record.emplace_datafield("540.0.0.", subfs={"a": "MOOC", "u": value, "2": "cc"})
 
     def visit_classification(self, value: dict, record: Marc21Metadata) -> None:
         """Process classification."""
-        # TODO
+        super().visit(value, record)
+
+    def visit_lifecycle(self, value: dict, record: Marc21Metadata) -> None:
+        """Process lifecycle."""
+        super().visit(value, record)
+
+    def visit_contribute(self, value: list, record: Marc21Metadata) -> None:
+        """Process contribute."""
+        for contribute in value:
+            description = contribute["date"]["description"]["langstring"]["#text"]
+            role = contribute["role"]["value"]["langstring"]["#text"]
+            entities = contribute["entity"]
+            self.entities += entities
+            for name in entities:
+                record.emplace_datafield("700.1.0.", subfs={"a": name, "4": role})
+            record.emplace_datafield("545.0.0.", subfs={"a": description})
